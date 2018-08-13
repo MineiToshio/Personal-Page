429b292edeea756ccbf3b74f05c7c6828e1281b6
Toshio Minei <sminei10@gmail.com>
2018-08-13 13:01:29 -0500

    mostrar redes sociales en el footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import IconMenu from './IconMenu';
 
 export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        
+        <IconMenu />
+        <div>Toshio Minei ©2018</div>
+
+        <style jsx>{`
+          footer {
+            padding: 20px;
+            display: flex;
+            flex-direction: columns;
+            align-items: center;
+            justify-content: center;
+            background: var(--green);
+            flex-direction: column;
+
+            div {
+              margin-top: 20px;
+            }
+          }
+        `}</style>
       </footer>
     )
   }
