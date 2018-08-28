580c25a88f89758ddb3b52a8c7c4844a4bfe64e7
Toshio Minei <sminei10@gmail.com>
2018-08-28 00:16:15 -0500

    smoothscroll por clase en vez de general

--- helpers/smoothScroll.js
+++ helpers/smoothScroll.js
@@ -1,5 +1,6 @@
 export default function smoothScroll() {
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  document.querySelectorAll('.scroll').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
           e.preventDefault();
 
