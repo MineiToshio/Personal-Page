c25bc0619a5b0662ed2db0c7ba00edc4d324cd4b
Toshio Minei <sminei10@gmail.com>
2018-08-29 17:20:54 -0500

    combio de home components a functional

--- components/home/Portada.jsx
+++ components/home/Portada.jsx
@@ -3,61 +3,58 @@ import VerMas from '../widgets/VerMas';
 import IconMenu from '../social-icons/IconMenu';
 import ParticleJs from '../external/ParticleJs';
 
-export default class Portada extends React.Component {
-
-  render () {
-    return (
-      <div className="portada">
-        <ParticleJs/>
-        <IconMenu />
-        <div className="content">
-          <div className="title">¡Bienvenido! Soy <span className="highlight">Toshio Minei</span></div>
-          <div className="sub-title">tech geek, emprendedor, lifelong learner</div>
-        </div>
-        <VerMas title="conoce mi trabajo" />
-
-        <style jsx>{`
-          .portada {
-            height: 100vh;
-            width: 100wh;
-            color: #fff;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: var(--green);
-
-            :global(.social-icons) {
-              position: absolute;
-              top: 0;
-              right: 0;
-              margin: 20px;
-
-              :global(a) {
-                color: var(--black);
-              }
-            }
-          }
-
-          .content {
-            text-align: center;
-            z-index: 1;
-
-            .title {
-              font-size: 30pt;
-              
-              .highlight {
-                color: var(--black);
-                font-weight: bold;
-              }
-            }
-
-            .sub-title {
-              font-size: 25pt;
-              padding-top: 15px;
-            }
-          }
-        `}</style>
+export default () => {
+  return (
+    <div className="portada">
+      <ParticleJs/>
+      <IconMenu />
+      <div className="content">
+        <div className="title">¡Bienvenido! Soy <span className="highlight">Toshio Minei</span></div>
+        <div className="sub-title">tech geek, emprendedor, lifelong learner</div>
       </div>
-    )
-  }
+      <VerMas title="conoce mi trabajo" />
+
+      <style jsx>{`
+        .portada {
+          height: 100vh;
+          width: 100wh;
+          color: #fff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: var(--green);
+
+          :global(.social-icons) {
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin: 20px;
+
+            :global(a) {
+              color: var(--black);
+            }
+          }
+        }
+
+        .content {
+          text-align: center;
+          z-index: 1;
+
+          .title {
+            font-size: 30pt;
+            
+            .highlight {
+              color: var(--black);
+              font-weight: bold;
+            }
+          }
+
+          .sub-title {
+            font-size: 25pt;
+            padding-top: 15px;
+          }
+        }
+      `}</style>
+    </div>
+  )
 }
--- components/home/Proyects.jsx
+++ components/home/Proyects.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import Portfolio from '../projects/Portfolio';
 import SectionFooter from '../widgets/SectionFooter';
 
-export default class Projects extends React.Component {
+export default (props) => {
+  const { projects } = props;
 
-  render () {
-    const { projects } = this.props;
+  return (
+    <div className="projects">
+      <Portfolio projects={projects} />
+      <SectionFooter 
+        title="¿quieres ver más proyectos?"
+        url="https://github.com/MineiToshio"
+        button="Visita mi Github" />
 
-    return (
-      <div className="projects">
-        <Portfolio projects={projects} />
-        <SectionFooter 
-          title="¿quieres ver más proyectos?"
-          url="https://github.com/MineiToshio"
-          button="Visita mi Github" />
-
-        <style jsx>{`
-          .projects {
-            width: 100%;
-          }
-        `}</style>
-      </div>
-    )
-  }
+      <style jsx>{`
+        .projects {
+          width: 100%;
+        }
+      `}</style>
+    </div>
+  )
 }
