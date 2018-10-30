5d216e9e63b02b70e275d9b231b3e7688f2675c9
Toshio Minei <sminei10@gmail.com>
2018-10-29 22:31:25 -0500

    addes post to routes

--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const routes = require('next-routes')
 // Name   Page      Pattern
 module.exports = routes()
   .add('index')
-  .add('blog', '/blog', 'blog')               
+  .add('blog', '/blog', 'blog')
+  .add('post', '/blog/:post', 'post')
