18757e012281a8fae6e28257b1863b12d33441d4
Toshio Minei <sminei10@gmail.com>
2018-10-29 22:31:40 -0500

    remove comments

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// const next = require('next')
-// const routes = require('./routes')
-// const app = next({ dev: process.env.NODE_ENV !== 'production' })
-// const handler = routes.getRequestHandler(app)
-// const port = process.env.PORT || 3000;
-// const { join } = require('path')
-// const { parse } = require('url')
-// const { createServer } = require('http')
-
-// app.prepare().then(() => {
-//   createServer((req, res) => {
-//     const parsedUrl = parse(req.url, true)
-//     const { pathname } = parsedUrl
-
-//     if (pathname === '/service-worker.js') {
-//       const filePath = join(__dirname, '.next', pathname)
-
-//       app.serveStatic(req, res, filePath)
-//     } else {
-//       handler(req, res, parsedUrl)
-//     }
-//   }).listen(port)
-// })
-
 const { join } = require('path');
 const express = require('express');
 const next = require('next');
