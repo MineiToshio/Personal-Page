64ac3489d34301c4314c336e83e122db3839cab0
Toshio Minei <sminei10@gmail.com>
2021-06-03 17:42:15 -0500

    validate url when inserting a new post

--- firebase/posts.ts
+++ firebase/posts.ts
@@ -2,6 +2,15 @@
 import db from './db';
 import type { PostDoc } from '@/types/firebase';
 
+export const getPostByUrl = async (url: string) => {
+  const snap = await db.posts.where('url', '==', url).get();
+  if (snap.docs.length === 0) return null;
+  const doc = snap.docs[0];
+  const data = doc.data();
+  const post = { id: doc.id, ...data };
+  return post;
+};
+
 export const getPosts = async () => {
   const snap = await db.posts.get();
   const posts = snap.docs.map(doc => {
@@ -15,6 +24,10 @@ export const getPosts = async () => {
 };
 
 export const createPost = async (post: Omit<PostDoc, 'id'>) => {
+  const postWithUrl = await getPostByUrl(post.url);
+
+  if (postWithUrl) throw new Error('Invalid url');
+
   const ref = await db.posts.add(post);
   return {
     ...post,
