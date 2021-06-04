5b290d1fd44f6ae9e63090814a63640de6931887
Toshio Minei <sminei10@gmail.com>
2021-06-04 01:18:09 -0500

    export default as variable for calculateReadingTime

--- helpers/calculateReadingTime.ts
+++ helpers/calculateReadingTime.ts
@@ -1,7 +1,9 @@
-export default (text?: string) => {
+const calculateReadingTime = (text?: string) => {
   if (!text) return 0;
   const wordsPerMinute = 225;
   const words = text.trim().split(/\s+/).length;
   const time = Math.ceil(words / wordsPerMinute);
   return time;
 }
+
+export default calculateReadingTime;
