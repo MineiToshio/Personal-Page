f69ae2032ded15efddcdbcaa83ab557054b80e97
Toshio Minei <sminei10@gmail.com>
2023-04-21 18:16:27 -0500

    chore: upgrade html-to-text

--- helpers/formatHtml.ts
+++ helpers/formatHtml.ts
@@ -4,7 +4,6 @@ import shortenText from './shortenText';
 const formatHtml = (html: string, charLength: number) => {
   const textWithoutHtml = removeHtml(html);
   const shortText = shortenText(textWithoutHtml, charLength);
-  console.log(shortText, 'shortText');
   return shortText;
 };
 
